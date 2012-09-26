1ca218aab2933a45db387885cf39ad4ba2d53d7a
Taha Hachana <tahahachana@live.com>
2012-09-26 11:59:56 +0100

    add seq module

---
+++ FStats/Seq.fs
@@ -0,0 +1,10 @@
+﻿namespace FStats
+
+module Seq =
+
+    let maximum = Statistics.maximum
+    let mean    = Statistics.mean
+    let median  = Statistics.median
+    let minimum = Statistics.minimum
+    let mode    = Statistics.mode        
+
