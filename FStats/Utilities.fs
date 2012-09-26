1f2465002efe8de2c06fe7b8abb6c65bd199f43b
Taha Hachana <tahahachana@live.com>
2012-09-26 11:59:16 +0100

    add utilities module

---
+++ FStats/Utilities.fs
@@ -0,0 +1,10 @@
+﻿namespace FStats
+
+module internal Utilities =
+    
+    let inline getIndex (dataset : seq<float>) x y =
+        float (Seq.length dataset * x) / y
+        |> ceil
+        |> int
+        |> (fun x -> x - 1)
+
