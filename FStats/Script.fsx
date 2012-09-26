1b5a671352e5e2fe5e7e77fa144d5e19b78b658e
Taha Hachana <tahahachana@live.com>
2012-09-26 12:02:30 +0100

    fix median, add max, min and mode

--- FStats/Script.fsx
+++ FStats/Script.fsx
@@ -1,14 +1,35 @@
-﻿#load "Module1.fs"
+﻿#r @"..\FStats\bin\Debug\FStats.dll"
 
 open System
 open FStats
 
 let rand = Random()
 
-let dataset =
-    [ for x in 0 .. 100000 do
+let data =
+    [ for x in 0 .. 1000000 do
         yield rand.NextDouble() * 10.
     ]
 
-let mean = Statistics.mean dataset
-let median = Statistics.median dataset
+let stats = DescriptiveStatistics.descriptiveStatistics data
+
+let maximum   = stats.Maximum
+let maximum'  = Statistics.maximum data
+let maximum'' = Seq.maximum data
+
+let mean   = stats.Mean
+let mean'  = Statistics.mean data
+let mean'' = Seq.mean data
+
+let median   = stats.Median
+let median'  = Statistics.median data
+let median'' = Seq.median data
+
+let minimum   = stats.Minimum
+let minimum'  = Statistics.minimum data
+let minimum'' = Seq.minimum data
+
+let mode   = stats.Mode
+let mode'  = Statistics.mode data
+let mode'' = Seq.mode data
+
+
---
+++ FStats/Statistics.fs
@@ -0,0 +1,44 @@
+﻿namespace FStats
+
+open Utilities
+
+module Statistics =
+    
+    /// <summary>Returns the maximum value in a data set.</summary>
+    /// <param name="data">The dat set.</param>
+    /// <returns>The maximum value.</returns>
+    let inline maximum (data: seq<float>) = Seq.max data
+
+    /// <summary>Calculates the mean of a data set.</summary>
+    /// <param name="data">The dat set.</param>
+    /// <returns>The mean value.</returns>
+    let inline mean (data: seq<float>) = Seq.average data
+
+    /// <summary>Calculates the median of a data set.</summary>
+    /// <param name="data">The dat set.</param>
+    /// <returns>The median value.</returns>        
+    let inline median (data: seq<float>) =
+        let isEven = Seq.length data % 2 = 0
+        let data' = Seq.sort data
+        let index = getIndex data 1 2.
+        match isEven with
+            | false -> Seq.nth index data'
+            | true  -> (Seq.nth index data' + Seq.nth (index + 1) data') / 2.            
+
+    /// <summary>Returns the minimum value in a data set.</summary>
+    /// <param name="data">The dat set.</param>
+    /// <returns>The minimum value.</returns>
+    let inline minimum (data : seq<float>) = Seq.min data
+
+    /// <summary>Returns the mode of a data set.</summary>
+    /// <param name="data">The dat set.</param>
+    /// <returns>The mode value(s) list.</returns>
+    let inline mode (data : seq<'T>) =
+        Seq.groupBy id data
+        |> Seq.groupBy (fun (_, x) -> Seq.length x)
+        |> Seq.maxBy fst
+        |> snd
+        |> Seq.map fst
+        |> Seq.toArray
+
+
