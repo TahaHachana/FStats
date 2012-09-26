41c2b219dc785a5bce3c385682f1046bebef7ddc
Taha Hachana <tahahachana@live.com>
2012-09-26 12:00:24 +0100

    add descriptivestatistics module

---
+++ FStats/DescriptiveStatistics.fs
@@ -0,0 +1,17 @@
+﻿namespace FStats
+
+open Types
+open Statistics
+
+module DescriptiveStatistics =
+
+    let descriptiveStatistics (data: seq<float>) =
+        {
+            Maximum = maximum data
+            Mean    = mean    data
+            Median  = median  data
+            Minimum = minimum data
+            Mode    = mode    data
+        }
+
+
