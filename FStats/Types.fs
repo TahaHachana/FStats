ae93c9afccbc4fdb9744780da8150caa7a63b752
Taha Hachana <tahahachana@live.com>
2012-09-26 11:59:35 +0100

    add types module

---
+++ FStats/Types.fs
@@ -0,0 +1,14 @@
+﻿namespace FStats
+
+module Types =
+
+    type DescriptiveStatistics =
+        {
+            Maximum : float
+            Mean    : float
+            Median  : float
+            Minimum : float
+            Mode    : float list
+        }
+
+
